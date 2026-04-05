110e978fb9222a5368acf3adac6b54c49414d403
abhinav-phi <alpha9coder@gmail.com>
2026-04-05 12:50:51 +0530

    test run

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {}
+    // 'process.env': {}
+    'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL ?? ''),
   }
 })
